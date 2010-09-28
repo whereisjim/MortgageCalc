aa35fee09c0900a34467b104e06454bf944a82b9
WhereIsJim <WhereIsJim@gmail.com>
2010-09-27 22:39:15 -0700

    Update for 2010/09/27

--- MortgageCalc/MainPage.xaml.cs
+++ MortgageCalc/MainPage.xaml.cs
@@ -10,6 +10,10 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.Globalization;
+using Microsoft.Phone;
+using Microsoft.Phone.Tasks;
+using System.Threading;
 
 namespace MortgageCalc
 {
@@ -33,5 +37,39 @@ namespace MortgageCalc
                 App.ViewModel.LoadData();
             }
         }
+
+        private void buttonMortgageCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            double principal; // total mortgage loan
+            double interestPerc; // percent annual interest
+            double interestRate; // monthly interest rate
+            double years; // years to pay
+            double downpayment; // down payment percent
+            double paymentNum; // number of months to pay
+            double paymentVal; // value of monthly payment
+
+            principal = double.Parse(this.textBoxMortgageAmount.Text, NumberStyles.Any);
+            interestPerc = double.Parse(this.textBoxMortgageInterestRate.Text);
+            interestRate = interestPerc / (100 * 12);
+            years = double.Parse(this.textBoxMortgageTerm.Text);
+            downpayment = double.Parse(this.textBoxMortgageDownPayment.Text);
+            paymentNum = years * 12;
+
+            paymentVal = (principal - principal * downpayment / 100) * (interestRate / (1 - Math.Pow((1 + interestRate), (-paymentNum))));
+
+            CultureInfo cult = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = cult;
+            this.textBoxMortgageMonthlyPayments.Text = paymentVal.ToString("C");
+            this.textBoxMortgageAmount.Text = principal.ToString("C");
+
+        }
+
+        private void buttonImage_Click(object sender, RoutedEventArgs e)
+        {
+            WebBrowserTask wbt = new WebBrowserTask();
+            wbt.URL = "http://www.amerisave.com";
+            wbt.Show();
+        }
+
     }
 }
